f6d3347e9a1b040a4e7c9b26905e53c13a70dda8
waddahbakil <42424111+waddahbakil@users.noreply.github.com>
2026-04-30 23:22:13 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/pharmacypdf/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.pharmacypdf
+import android.app.Activity
+import android.os.Bundle
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
